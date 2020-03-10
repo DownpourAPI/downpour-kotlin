fbabd9f6b558e08aa6231036b170eedba16e9356
Peter Hanrahan <ptr@hnrhn.com>
2020-03-10 15:35:54 -0300

    Re-order payload params for serialization

--- src/main/kotlin/models/PauseTorrentPayload.kt
+++ src/main/kotlin/models/PauseTorrentPayload.kt
@@ -4,14 +4,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PauseTorrentPayload(
-    val params: List<List<String>>,
     val id: Int = 1,
-    val method: String = "core.pause_torrent"
+    val method: String = "core.pause_torrent",
+    val params: List<List<String>>
 ) {
     companion object {
         fun defaultPayload(torrentHash: String): PauseTorrentPayload {
             val params = listOf(listOf(torrentHash))
-            return PauseTorrentPayload(params)
+            return PauseTorrentPayload(params = params)
         }
     }
 }
