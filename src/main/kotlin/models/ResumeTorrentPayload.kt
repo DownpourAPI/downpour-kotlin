faa4641878e79652c644c9130e09659c8ac3f2d3
Peter Hanrahan <ptr@hnrhn.com>
2020-03-12 12:45:22 -0300

    Fix order of Resume properties

--- src/main/kotlin/models/ResumeTorrentPayload.kt
+++ src/main/kotlin/models/ResumeTorrentPayload.kt
@@ -4,14 +4,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResumeTorrentPayload(
-    val params: List<List<String>>,
     val id: Int = 1,
-    val method: String = "core.resume_torrent"
+    val method: String = "core.resume_torrent",
+    val params: List<List<String>>
 ) {
     companion object {
         fun defaultPayload(torrentHash: String): ResumeTorrentPayload {
             val params = listOf(listOf(torrentHash))
-            return ResumeTorrentPayload(params)
+            return ResumeTorrentPayload(params = params)
         }
     }
 }
