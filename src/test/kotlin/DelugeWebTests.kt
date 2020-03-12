397d77c8b9ed95b4c4e9b45311fb7f7d43c9b2e2
Peter Hanrahan <ptr@hnrhn.com>
2020-03-12 13:19:02 -0300

    Re-enable GetAll content unit test

--- src/test/kotlin/DelugeWebTests.kt
+++ src/test/kotlin/DelugeWebTests.kt
@@ -45,25 +45,25 @@ class DelugeWebTests {
             }
         }
 
-//        @Test
-//        fun `response is parsed correctly - returns a List of Torrents`() {
-//            val client = mockk<Client>()
-//            val returnedJson = """{"id":1,"result":{"stats":{"upload_protocol_rate":78799,"max_upload":-1.0,"download_protocol_rate":75432,"download_rate":2353262,"has_incoming_connections":true,"num_connections":4,"max_download":-1.0,"upload_rate":0,"dht_nodes":386,"free_space":3419688734720,"max_num_connections":50},"connected":true,"torrents":{"hash1":{"max_download_speed":-1.0,"upload_payload_rate":0,"download_payload_rate":2353262,"num_peers":0,"ratio":0.0,"total_peers":2,"max_upload_speed":-1.0,"state":"Downloading","distributed_copies":2.0220000743865967,"save_path":"/save/directory/path","progress":100.0,"time_added":1582554880.0,"tracker_host":"tracker.host","total_uploaded":0,"total_done":40370254,"total_wanted":1771415839,"total_seeds":6,"seeds_peers_ratio":3.0,"num_seeds":2,"name":"Test Torrent One","is_auto_managed":true,"queue":0,"eta":735},"hash2":{"max_download_speed":-1.0,"upload_payload_rate":0,"download_payload_rate":2353262,"num_peers":0,"ratio":0.0,"total_peers":2,"max_upload_speed":-1.0,"state":"Downloading","distributed_copies":2.0220000743865967,"save_path":"/save/directory/path","progress":97.4,"time_added":1582554880.0,"tracker_host":"tracker.host","total_uploaded":0,"total_done":40370254,"total_wanted":1771415839,"total_seeds":6,"seeds_peers_ratio":3.0,"num_seeds":2,"name":"Test Torrent Two","is_auto_managed":true,"queue":0,"eta":735}},"filters":{"state":[["All",1],["Downloading",1],["Seeding",0],["Active",1],["Paused",0],["Queued",0],["Checking",0],["Error",0]],"tracker_host":[["All",1],["Error",0],["tracker.com",1]]}},"error":null}"""
-//            every { client.executeRequest(any()).statusCode } returns 200
-//            every { client.executeRequest(any()).responseMessage } returns "OK"
-//            every { client.executeRequest(any()).data } returns returnedJson.toByteArray(Charsets.UTF_8)
-//            FuelManager.instance.client = client
-//
-//            val expected = listOf(
-//                Torrent("hash1", 100.0, "Test Torrent One"),
-//                Torrent("hash2", 97.4, "Test Torrent Two")
-//            )
-//
-//            val actual = testSession.getAllTorrents()
-//
-//            assertThat(actual)
-//                .containsAll(expected)
-//        }
+        @Test
+        fun `response is parsed correctly - returns a List of Torrents`() {
+            val client = mockk<Client>()
+            val returnedJson = """{"id":1,"result":{"stats":{"upload_protocol_rate":78799,"max_upload":-1.0,"download_protocol_rate":75432,"download_rate":2353262,"has_incoming_connections":true,"num_connections":4,"max_download":-1.0,"upload_rate":0,"dht_nodes":386,"free_space":3419688734720,"max_num_connections":50},"connected":true,"torrents":{"hash1":{"max_download_speed":-1.0,"upload_payload_rate":0,"download_payload_rate":2353262,"num_peers":0,"ratio":0.0,"total_peers":2,"max_upload_speed":-1.0,"state":"Downloading","distributed_copies":2.0220000743865967,"save_path":"/save/directory/path","progress":100.0,"time_added":1582554880.0,"tracker_host":"tracker.host","total_uploaded":0,"total_done":40370254,"total_wanted":1771415839,"total_seeds":6,"seeds_peers_ratio":3.0,"num_seeds":2,"name":"Test Torrent One","is_auto_managed":true,"queue":0,"eta":735},"hash2":{"max_download_speed":-1.0,"upload_payload_rate":0,"download_payload_rate":2353262,"num_peers":0,"ratio":0.0,"total_peers":2,"max_upload_speed":-1.0,"state":"Downloading","distributed_copies":2.0220000743865967,"save_path":"/save/directory/path","progress":97.4,"time_added":1582554880.0,"tracker_host":"tracker.host","total_uploaded":0,"total_done":40370254,"total_wanted":1771415839,"total_seeds":6,"seeds_peers_ratio":3.0,"num_seeds":2,"name":"Test Torrent Two","is_auto_managed":true,"queue":0,"eta":735}},"filters":{"state":[["All",1],["Downloading",1],["Seeding",0],["Active",1],["Paused",0],["Queued",0],["Checking",0],["Error",0]],"tracker_host":[["All",1],["Error",0],["tracker.com",1]]}},"error":null}"""
+            every { client.executeRequest(any()).statusCode } returns 200
+            every { client.executeRequest(any()).responseMessage } returns "OK"
+            every { client.executeRequest(any()).data } returns returnedJson.toByteArray(Charsets.UTF_8)
+            FuelManager.instance.client = client
+
+            val expected = listOf(
+                Torrent("hash1", 100.0, "Test Torrent One"),
+                Torrent("hash2", 97.4, "Test Torrent Two")
+            )
+
+            val actual = testSession.getAllTorrents()
+
+            assertThat(actual)
+                .containsAll(expected)
+        }
 
         @Test
         fun `request rejection is handled`() {
