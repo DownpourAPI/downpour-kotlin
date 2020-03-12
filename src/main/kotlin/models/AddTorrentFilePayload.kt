8b5ec7f9f3846607011b948ebe893037403a814c
Peter Hanrahan <ptr@hnrhn.com>
2020-03-12 17:06:07 -0300

    Fix AddTorrentFile string generation

--- src/main/kotlin/models/AddTorrentFilePayload.kt
+++ src/main/kotlin/models/AddTorrentFilePayload.kt
@@ -1,6 +1,5 @@
 package models
 
-import kotlinx.serialization.Serializable
 import java.io.File
 import java.util.*
 
@@ -8,6 +7,6 @@ class AddTorrentFilePayload(torrentFile: File) {
     private val fileDump: String = Base64.getEncoder().encodeToString(torrentFile.readBytes())
 
     override fun toString(): String {
-        return """{"id":1, "method":"core.add_torrent_file", "params":["file",$fileDump,{}]}"""
+        return """{"id":1, "method":"core.add_torrent_file", "params":["file","$fileDump",{}]}"""
     }
 }
