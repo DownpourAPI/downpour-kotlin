d240d099d0101b1221576e0798a7e2848fd89b3d
Peter Hanrahan <ptr@hnrhn.com>
2020-03-26 14:09:06 -0300

    Remove unnecessary string manipulation

--- src/main/kotlin/com/hnrhn/downpour/impl/deluge/jsonobjects/RemoveTorrentPayload.kt
+++ src/main/kotlin/com/hnrhn/downpour/impl/deluge/jsonobjects/RemoveTorrentPayload.kt
@@ -2,6 +2,6 @@ package com.hnrhn.downpour.impl.deluge.jsonobjects
 
 class RemoveTorrentPayload(private val torrentHash: String, private val withData: Boolean) {
     override fun toString(): String {
-        return """{"id":1,"method":"core.remove_torrent","params":["$torrentHash",${withData.toString().toLowerCase()}]}"""
+        return """{"id":1,"method":"core.remove_torrent","params":["$torrentHash",$withData]}"""
     }
 }
