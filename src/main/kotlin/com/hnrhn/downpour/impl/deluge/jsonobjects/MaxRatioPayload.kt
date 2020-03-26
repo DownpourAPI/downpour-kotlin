b1c7ef14d67a5275ded59e03d06c9265cb811db8
Peter Hanrahan <ptr@hnrhn.com>
2020-03-25 23:49:40 -0300

    Switch all payloads to toString() method

--- src/main/kotlin/com/hnrhn/downpour/impl/deluge/jsonobjects/MaxRatioPayload.kt
+++ src/main/kotlin/com/hnrhn/downpour/impl/deluge/jsonobjects/MaxRatioPayload.kt
@@ -1,25 +1,7 @@
 package com.hnrhn.downpour.impl.deluge.jsonobjects
 
-data class MaxRatioPayload(
-    val maxRatioParams: List<Any>,
-    val id: Int = 1,
-    val method: String  = "core.set_torrent_options"
-    ) {
-
+class MaxRatioPayload(private val torrentHash: String, private val ratio: Int) {
     override fun toString(): String {
-        return """{"id": ${id}, "method": "$method", "params": [["${(maxRatioParams[0] as List<*>)[0]}"], {"stop_at_ratio": ${(maxRatioParams[1] as HashMap<*, *>)["stop_at_ratio"]}, "stop_ratio": ${(maxRatioParams[1] as HashMap<*, *>)["stop_ratio"]}}]}"""
-    }
-
-    companion object Factory {
-        fun defaultPayload(torrentHash: String, maxRatio: Int): MaxRatioPayload {
-            val maxRatioParams = listOf(
-                listOf(torrentHash),
-                hashMapOf("stop_at_ratio" to true, "stop_ratio" to maxRatio)
-            )
-
-            return MaxRatioPayload(
-                maxRatioParams
-            )
-        }
+        return """{"id":1,"method":"core.set_torrent_options","params":[["$torrentHash"],{"stop_at_ratio":true,"stop_ratio":$ratio}]}"""
     }
 }
--- src/main/kotlin/com/hnrhn/downpour/impl/deluge/jsonobjects/RemoveTorrentPayload.kt
+++ src/main/kotlin/com/hnrhn/downpour/impl/deluge/jsonobjects/RemoveTorrentPayload.kt
@@ -1,20 +1,7 @@
 package com.hnrhn.downpour.impl.deluge.jsonobjects
 
-import kotlinx.serialization.ContextualSerialization
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class RemoveTorrentPayload(
-    val params: List<@ContextualSerialization Any>,
-    val id: Int = 1,
-    val method: String = "core.remove_torrent"
-
-) {
-    companion object Factory {
-        fun defaultPayload(torrentHash: String, withData: Boolean): RemoveTorrentPayload {
-            return RemoveTorrentPayload(
-                listOf(torrentHash, withData)
-            )
-        }
+class RemoveTorrentPayload(private val torrentHash: String, private val withData: Boolean) {
+    override fun toString(): String {
+        return """{"id":1,"method":"core.remove_torrent","params":["$torrentHash",${withData.toString().toLowerCase()}]}"""
     }
 }
